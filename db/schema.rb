26ab56b1d2bfbe8a11a0b25ac9e62f251ec46eb3
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-01 15:28:18 +0500

    add column in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_082834) do
+ActiveRecord::Schema.define(version: 2022_09_01_102552) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
