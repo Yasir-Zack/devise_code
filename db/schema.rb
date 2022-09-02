8f5a826f33b044340fb3c85643feccdc880bed93
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-02 12:39:20 +0500

    migration for user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_102552) do
+ActiveRecord::Schema.define(version: 2022_09_02_073758) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "body"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
